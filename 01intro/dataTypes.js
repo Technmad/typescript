7c07332d950b9279c47c2d57de4e41b5d5948ec4
anurag <anuragpathakoffl@gmail.com>
2023-08-20 12:23:17 +0530

    data types

---
+++ 01intro/dataTypes.js
@@ -0,0 +1,10 @@
+// primitive data types
+// string
+var userName = "Anurag";
+// number
+var userId = 1001;
+// boolean
+var isLoggedIn = false;
+console.log(userName);
+console.log(userId);
+console.log(isLoggedIn);
---
+++ 01intro/dataTypes.ts
@@ -0,0 +1,15 @@
+// primitive data types
+// string
+let userName: string = "Anurag";
+
+// number
+let userId: number = 1001;
+
+// boolean
+let isLoggedIn = false;
+
+console.log(userName);
+console.log(userId);
+console.log(isLoggedIn);
+
+export {};
